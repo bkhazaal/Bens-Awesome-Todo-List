2462251cb088f55b4a0913322c9ef89bcbc67999
bkhazaal <thajukez101@gmail.com>
2024-06-13 13:04:09 -0700

    first-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,137 @@
+import { nanoid } from "nanoid";
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
+interface Todo {
+  id: string;
+  text: string;
+  completed: boolean;
+}
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [todoItems, setTodoItems] = useState<Todo[]>([]);
+  const [input, setInput] = useState("");
+  const [view, setView] = useState<"all" | "active" | "completed">("all");
+
+  const addNewToDo = (text: string) => {
+    setInput("");
+    if (text.length === 0) {
+      return false;
+    }
+    const newTodo: Todo = {
+      id: nanoid(),
+      text,
+      completed: false,
+    };
+
+    console.log("newTodo", newTodo);
+
+    setTodoItems((prev) => [...prev, newTodo]);
+  };
+
+  const deleteTodos = (id: string) => {
+    const newTodoItems = todoItems.filter((todo) => todo.id !== id);
+    setTodoItems(newTodoItems);
+  };
+
+  const markAsCompleted = (id: string, completed: boolean) => {
+    console.log(completed);
+    const updatedTodoItems = todoItems.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      } else {
+        return todo;
+      }
+    });
+    setTodoItems(updatedTodoItems);
+  };
+
+  const clearCompleted = () => {
+    const completedItems = todoItems.filter((todo) => todo.completed === false);
+    setTodoItems(completedItems);
+  };
+
+  const active = todoItems.filter((todo) => todo.completed === false);
+
+  // if the clear completed button is pressed, all of the todo cards that are checked (true) will be completely deleted //
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" rel="noopener noreferrer" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <div className="container">
+      <header className="header-container"></header>
+      <div className="the-entire-ui">
+        <div className="header-items">
+          <h1>T O D O</h1>
+          <button type="button">
+            <img src="/icon-moon.svg" alt="Moon" />
+          </button>
+        </div>
+        <div className="todolist">
+          <div className="create-todo-box">
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.currentTarget.value)}
+              placeholder="Create a new todo..."
+              onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+                if (e.key === "Enter")
+                  addNewToDo(
+                    e.currentTarget.value.charAt(0).toUpperCase() +
+                      input.slice(1)
+                  );
+              }}
+            />
+          </div>
+          <div className="todo-container">
+            <div className="input-fields-container">
+              {todoItems
+                .filter((todo) => {
+                  if (view === "completed") {
+                    return todo.completed === true;
+                  } else if (view === "active") {
+                    return todo.completed === false;
+                  } else {
+                    return todo;
+                  }
+                })
+                .map((todo) => (
+                  <div key={todo.id} className="todo-field">
+                    <div className="checkbox-container">
+                      {/* Add a function on the checkbox that marks the item as completed */}
 
-      <h1>Vite + React</h1>
+                      <input
+                        type="checkbox"
+                        checked={todo.completed}
+                        onChange={(event) =>
+                          markAsCompleted(todo.id, event.currentTarget.checked)
+                        }
+                      />
+                    </div>
+                    <p
+                      className={`"todolist-text" ${todo.completed ? "completedText" : ""}`}
+                    >
+                      {todo.text}
+                    </p>
+                    <button onClick={() => deleteTodos(todo.id)}>X</button>
+                  </div>
+                ))}
 
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+              <div className="todo-field">
+                <p>{active.length} Items left</p>
+                <button onClick={clearCompleted}>Clear Completed</button>
+              </div>
+            </div>
+            <div className="status-buttons todo-field">
+              <button onClick={() => setView("all")}>All</button>
+              <button onClick={() => setView("active")}>Active</button>
+              <button onClick={() => setView("completed")}>Completed</button>
+            </div>
+            <p className="error-message" hidden>
+              Please Enter Something
+            </p>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
